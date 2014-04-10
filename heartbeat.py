47da01e12741f9801a64af9d9ad5a522c34360ca
forest <luhaoforest@gmail.com>
2014-04-10 14:23:41 +0800

    add node heartbeat

---
+++ heartbeat.py
@@ -0,0 +1,18 @@
+import urllib2
+import socket
+socket.setdefaulttimeout(5)
+
+from config import redis_client
+
+for _node in redis_client.smembers("nodes"):
+    node = eval(_node)
+    url = "http://%s:%d/heartbeat" % (node['ip'], node['port'])
+    try:
+        content = urllib2.urlopen(url).read()
+        if content != "don't shoot!your friend!":
+            redis_client.srem(_node, "nodes")
+    except:
+        redis_client.srem("nodes", _node)
+
+
+
